345a81278ad6f836eab525af484abf377dd27ac6
adrianczesnowski <adrian.czesnowski22@gmail.com>
2021-04-23 21:07:55 +0200

    inputNumber placeholder

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,30 +2,35 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './Input.module.css';
 
-const Input = ({ tag: Tag, name, type, value, handleChange, placeholder }) => (
-  <>
-    {Tag === 'textarea' ? (
-      <textarea
-        className={styles.Textarea}
-        name={name}
-        id={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-        type={type}
-      />
-    ) : (
-      <input
-        className={styles.Input}
-        name={name}
-        id={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-        type={type}
-      />
-    )}
-  </>
-);
+const Input = ({ tag: Tag, name, type, value, handleChange, placeholder }) => {
+  //placeholder for input:number
+  const inputNumberValue = value === 0 ? '' : value;
+
+  return (
+    <>
+      {Tag === 'textarea' ? (
+        <textarea
+          className={styles.Textarea}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+          type={type}
+        />
+      ) : (
+        <input
+          className={styles.Input}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          value={type === 'number' ? inputNumberValue : value}
+          onChange={handleChange}
+          type={type}
+        />
+      )}
+    </>
+  );
+};
 
 export default Input;
