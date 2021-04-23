e9edecd4328845ab251d073949548fbbf60e1e08
adrianczesnowski <adrian.czesnowski22@gmail.com>
2021-04-23 18:22:31 +0200

    adding comments to code

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,33 +1,50 @@
+// algorytm  obsługuje kody ascii z przedziału: 32 - 126 (małe, duże litery, liczby, znaki interpunkcyjne, spacje)
+// the algorithm supports ascii codes in the range: 32 - 126 (lowercase, uppercase letters, numbers, punctuation marks, spaces
+
+//cipher
 export function algorithmCipher(text, shift) {
   let word = '';
+
+  //variable to hold the current character in ascii format
   let x = 0;
+
   const shiftBy = parseInt(shift);
 
+  //iteration over each character
   for (let i = 0; i < text.length; i++) {
     x = String(text.charCodeAt(i));
 
+    //checking if the character is in the range from 32 to 126
     if (x >= 32 && x <= 126) {
       x = ((x - 32 + shiftBy) % 97) + 32;
     }
 
+    //creating a coded word
     word = word + String.fromCharCode(x);
   }
 
   return word;
 }
 
+//decipher
 export function algorithmDecipher(text, shift) {
   let word = '';
+
+  //variable to hold the current character in ascii format
   let x = 0;
+
   const shiftBy = parseInt(shift);
 
+  //iteration over each character
   for (let i = 0; i < text.length; i++) {
     x = String(text.charCodeAt(i));
 
+    //checking if the character is in the range from 32 to 126
     if (x >= 32 && x <= 126) {
       x = ((x - 32 - shiftBy) % 97) + 32;
     }
 
+    //creating a decoded word
     word = word + String.fromCharCode(x);
   }
 
